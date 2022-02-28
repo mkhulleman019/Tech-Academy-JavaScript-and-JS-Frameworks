5d4142cfa738a04332b3b415a52c12de740ee408
mkhulleman019 <mkhulleman019@yahoo.com>
2022-02-27 20:20:33 -0700

    Added getArea method for each shape

--- subclasses/js/jsSubclasses.js
+++ subclasses/js/jsSubclasses.js
@@ -3,29 +3,45 @@ class shape {
         this.color = color;
     }
     getArea(){
-        return "getting area of shape";
+        return "";
     }
 }
 
 class rectangle extends shape{
-    constructor(color){
+    constructor(color, height, width){
         super(color);
         this.height =  height;
         this.width = width;
     }
+    getArea(width, height){
+       return this.width * this.height;
+    }
 }
+let rectangle1 = new rectangle("blue", 4, 5);
+console.log(rectangle1.getArea());
+
 
 class triangle extends shape{
-    constructor(color){
+    constructor(color, base, height){
         super(color);
         this.base = base;
         this.height = height;
     }
+    getArea(base, height){
+        return (.5 * this.base) * this.height;
+    }  
 }
+let triangle1 = new triangle("red", 6, 6);
+console.log(triangle1.getArea());
 
 class circle extends shape{
-    constructor(color){
+    constructor(color, radius){
         super(color);
         this.radius = radius;
     }
+    getArea(radius){
+        return 3.1415 * (this.radius ** 2);
+    }
 }
+let circle1 = new circle("yellow", 5);
+console.log(circle1.getArea());
